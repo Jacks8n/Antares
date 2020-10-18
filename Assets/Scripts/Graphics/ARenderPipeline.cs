8e15fe71f7929e06d73bf31aed478c07c1e9ef33
Jacks0n <cjdxz10@126.com>
2020-10-18 23:31:07 +0800

    Add SRP

---
+++ Assets/Scripts/Graphics/ARenderPipeline.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ARenderPipeline : RenderPipeline
+{
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Graphics/ARenderPipelineAsset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/ARenderPipelineAsset")]
+public class ARenderPipelineAsset : RenderPipelineAsset
+{
+    protected override RenderPipeline CreatePipeline()
+    {
+        return new ARenderPipeline();
+    }
+}
